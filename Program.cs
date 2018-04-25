d48f54ef280915a5ea6ca5f8659b24c6a8130301
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2018-04-25 00:24:47 -0400

    Make it easier to transform files

--- Program.cs
+++ Program.cs
@@ -12,17 +12,7 @@ namespace XsltTransformer
    {
       public static void Main(string[] args)
       {
-         var xmlReaderSettings = new XmlReaderSettings();
-         xmlReaderSettings.DtdProcessing = DtdProcessing.Parse;
-
-         XmlReader xmlReader = XmlReader.Create( "../../fuel.xml", xmlReaderSettings );
-
-         XmlWriter xmlWriter = XmlWriter.Create( "../../fuel.html") ;
-
-         var xslt = new XslCompiledTransform();
-         xslt.Load( "../../print.xsl" );
-         //xslt.Transform( "../../fuel.xml", "../../fuel.html" );
-         xslt.Transform( xmlReader, xmlWriter );
+         XmlHelpers.XmlXsltTransform( "../../print.xsl", "../../fuel.xml", "../../output.html" );
       }
    }
 }
---
+++ XmlHelpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace XsltTransformer
+{
+   public static class XmlHelpers
+   {
+      public static XmlReader GetXmlReader( string pathToXml )
+      {
+         var xmlReaderSettings = new XmlReaderSettings();
+         xmlReaderSettings.DtdProcessing = DtdProcessing.Parse;
+
+         return XmlReader.Create( pathToXml, xmlReaderSettings );
+      }
+
+      public static XmlWriter GetXmlWriter( string pathToOutput )
+      {
+         return XmlWriter.Create( pathToOutput );
+      }
+
+      public static void XmlXsltTransform( string pathToXsl, string pathToXmlInput, string pathToOutput )
+      {
+         var xslt = new XslCompiledTransform();
+         xslt.Load( pathToXsl );
+         xslt.Transform( GetXmlReader( pathToXmlInput ), GetXmlWriter( pathToOutput ) );
+      }
+   }
+}
