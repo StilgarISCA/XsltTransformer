d6396ecdee89f920ef493c0badaedbcc3338794d
Glenn Hoeppner <glenn.hoeppner@yakhair.com>
2018-04-28 16:32:20 -0400

    Helper for output files/folder

--- FileHelpers.cs
+++ FileHelpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ namespace XsltTransformer
 {
    public static class FileHelpers
    {
+      public static string OutputPath = @"../../../Transformed/";
+
       public static IEnumerable<string> GetIssueContentsAtPath( string pathToFolder )
       {
          return Directory.EnumerateFiles( pathToFolder, "*.xml" ).Where( f => (!f.EndsWith( "cover.xml" ))  );
@@ -21,6 +24,17 @@ namespace XsltTransformer
          var matchYyyyMmDdNames = new Regex("[0-9]{8}");
 
          return (from dir in Directory.GetDirectories( pathToSearch ) where matchYyyyMmDdNames.IsMatch( dir ) select dir);
+      }
+
+      public static string SetUpOutputPath( string articlePath )
+      {
+         string fileName = Path.GetFileNameWithoutExtension( articlePath );
+         string issueDate = Path.GetDirectoryName( articlePath ).Split( Path.DirectorySeparatorChar ).Last();
+         string issueOutputPath = $"{OutputPath}{issueDate}";
+
+         Directory.CreateDirectory( issueOutputPath );
+
+         return $"{issueOutputPath}/{fileName}.html";
       }
    }
 }
